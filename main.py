d0cf93719df355754b5c2fc696afcca7b26def29
oupeiyivn-ctrl <oupeiyivn@gmail.com>
2026-07-20 08:28:35 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+WATCHLIST = [
+    "QQQ",
+    "SPY",
+    "TSM",
+    "AVGO",
+    "NBIS"
+]
+
+print("📈 Peiyi AI Investor")
+print("----------------------")
+
+for stock in WATCHLIST:
+    print(f"Tracking: {stock}")
+
+print("----------------------")
+print("System Ready ✅")
